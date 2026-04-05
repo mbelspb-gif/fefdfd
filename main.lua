9df105d0c585275c90fa62a150fe655f095bf4a5
mbelspb-gif <mbelspb@gmail.com>
2026-04-05 19:13:55 +0300

    Update and rename main.lua to Swordware.net

--- main.lua
+++
@@ -1,4 +0,0 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xBac0nBl1tzX/Teleport/refs/heads/main/Teleport-Tools.luau"))()
